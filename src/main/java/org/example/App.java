6afd2f01e2aaae0cb6a167e2998f22fecf9d881e
kathan219 <kathansoni@Kathans-MacBook-Air.local>
2024-02-29 09:34:49 +0530

    Final Code Addition

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        StringBuilder sb = new StringBuilder();
+        for (int e = 1; e <=100 ; e++) {
+            String m = fizbuz(e);
+            sb.append(m);
+            if(e<100){
+                sb.append(",");
+            }
+        }
+        System.out.println(sb);
     }
 
     public static String fizbuz(int i) {
